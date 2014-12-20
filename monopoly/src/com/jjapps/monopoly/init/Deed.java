f5b280153857303f87d6b3e08c414cb0098dc97e
jmeixner <jmeixner@uwm.edu>
2014-12-20 17:25:46 -0600

    made a deed enum need to merge

--- monopoly/src/com/jjapps/monopoly/init/Deed.java
+++ monopoly/src/com/jjapps/monopoly/init/Deed.java
@@ -3,12 +3,16 @@ package com.jjapps.monopoly.init;
 public abstract class Deed {
 	
 	private int price;
-	private int groupNumber;
+	private int spaceNumber;
+	private Group group;
 	private Player owner;
 	
 	public int getRent(){
 		if (owner == null) throw new IllegalStateException();
 		return determinePayment(ownsGroup());
+	}
+	
+	public void createDeed(Group grp){
 		
 	}
 	
---
+++ monopoly/src/com/jjapps/monopoly/init/Group.java
@@ -0,0 +1,7 @@
+package com.jjapps.monopoly.init;
+
+public enum Group {
+	PURPLE,LIGHT_BLUE,LAVENDER,ORANGE,RED,YELLOW,GREEN,DARK_BLUE,UTILLITY,RAIL_ROAD;
+	
+	
+}
