516d8673857f12371bca87684c407e7ac28d9a32
john <jrhayman@uwm.edu>
2014-12-20 17:34:18 -0600

    my inital commit <john>

--- monopoly/src/com/jjapps/monopoly/init/DeedSet.java
+++ monopoly/src/com/jjapps/monopoly/init/DeedSet.java
@@ -1,16 +1,42 @@
 package com.jjapps.monopoly.init;
 
-import java.util.TreeSet;
+import java.util.AbstractSet;
+import java.util.Iterator;
 
-public class DeedSet extends TreeSet<Deed> {
+
+public class DeedSet extends AbstractSet{
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private class Node{
+		private Group grp;
+		private Deed[] deedgrp;
+	}
+	public DeedSet(){
+		
+	}
+	public void Add(){
+		
+	}
 	
 	public Deed[] getGroup(){
-		this.
+		
 	}
+
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
 	
 }
